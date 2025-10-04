818e05e5e89a169f5c5223d6404fef8896d86d04
The Radian <yunusyunusebru@gmail.com>
2025-10-04 12:13:52 +0000

    herşeyi sıfırdan yap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import Header from '@/components/header';
-import MapLoader from '@/components/map-loader';
-
 export default function Home() {
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <Header />
-      <main className="relative flex-1">
-        <MapLoader />
-      </main>
-    </div>
+    <main>
+      <p>Get started by editing <code>src/app/page.tsx</code></p>
+    </main>
   );
 }
