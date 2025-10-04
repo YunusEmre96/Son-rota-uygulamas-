0ba39005244e57a4c7f56d864f6cbfebb0213f33
The Radian <yunusyunusebru@gmail.com>
2025-10-04 12:22:36 +0000

    her şeyi sil en baştan başayacağız

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import MapLoader from '@/components/map-loader';
-
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
-      <MapLoader />
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
     </main>
   );
 }
